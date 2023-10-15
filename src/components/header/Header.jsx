f4910238ee63fc9612030fc431e90d7d20bc1dbf
Vishnu <vishnu_ot@yahoo.com>
2023-10-15 19:38:13 +0530

    added cart head

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import "./Header.css";
 function Header() {
+  const { cart } = useSelector((state) => state.cart);
+  let totalCount = cart.reduce((acc, curr) => {
+    return acc + curr.count;
+  }, 0);
+
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <h3>Shpping Cart</h3>
+      <h3 style={{ marginRight: "100px" }}>Total Cart items: {totalCount}</h3>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
